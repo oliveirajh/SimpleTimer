a0ea70dec8ad245a6a68a1b8219954c25f39a3b1
João Henrique de Oliveira <68037457+oliveirajh@users.noreply.github.com>
2023-06-24 17:35:06 -0300

    Some changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,42 @@
-function getTimeFromSeconds(segundos){
-    const data = new Date(segundos * 1000);
-    return data.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        timeZone: 'GMT'
+function relogio(){
+    const relogio = document.querySelector('.timer');
+    let segundos = 0
+    let timer
+
+    function getTimeFromSeconds(segundos){ //Você passa os segundos e converte a quantidade de segundos em hora
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'GMT'
+        })
+    }
+
+    function iniciaRelogio(){ //Função para incrementar a variavel segundos a cada segundo usando setInterval
+        timer = setInterval(function(){
+            segundos++
+            relogio.innerHTML = getTimeFromSeconds(segundos);
+        }, 1000)
+    }
+
+    document.addEventListener('click', function(e){ //Função para obter elemento pelo click
+        const el = e.target // atribui o e.target a constante el
+        
+        if(el.classList.contains('iniciar')){  //Verifica se o elemento selecionado tem alguma classe que tenha iniciar e realiza as funções
+            clearInterval(timer);
+            iniciaRelogio();
+            relogio.classList.remove('paused')
+        }
+
+        if(el.classList.contains('pausar')){ //Verifica se o elemento selecionado tem alguma classe que tenha pausar e realiza as funções
+            clearInterval(timer);
+            relogio.classList.add('paused')
+        }
+        
+        if(el.classList.contains('zerar')) { //Verifica se o elemento selecionado tem alguma classe que tenha zerar e realiza as funções
+            segundos = 0;
+            relogio.innerHTML = getTimeFromSeconds(segundos);
+        }
     })
+
 }
-
-const relogio = document.querySelector('.timer');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
-let segundos = 0
-let timer
-
-function iniciaRelogio(){
-    timer = setInterval(function(){
-        segundos++
-        relogio.innerHTML = getTimeFromSeconds(segundos);
-    }, 1000)
-}
-
-iniciar.addEventListener('click', function(event){
-    iniciaRelogio();
-    iniciar.disabled = true
-    pausar.disabled = false
-    relogio.style.color = 'black'
-})
-
-pausar.addEventListener('click', function(event){
-    clearInterval(timer);
-    pausar.disabled = true
-    iniciar.disabled = false
-    relogio.style.color = 'red';
-})
-
-zerar.addEventListener('click', function(event){
-    segundos = 0;
-    relogio.innerHTML = getTimeFromSeconds(segundos);
-})
+relogio();
